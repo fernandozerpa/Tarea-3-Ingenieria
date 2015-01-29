16c5292d2beae3b0559909d64c2af49d9d43c810
fernandozerpa <zerpa.fernando48@gmail.com>
2015-01-29 00:16:48 -0430

    Agregadas validaciones de formato d hora y rango de horas validas.

--- Tarea3/marzullo.py
+++ Tarea3/marzullo.py
@@ -4,13 +4,30 @@
            Alejandra Preciado 07-41384
 '''
 import functools
+import datetime
 
 class Marzullo: 
+    
+    def formatoHoraValida(self, hora):
+        try:
+            datetime.datetime.strptime(hora, '%H:%M')
+        except ValueError:
+            raise ValueError("El formato de hora es incorrecto , el formato es 'hh:mm'")
 
+    def horaValida(self, hora):
+        
+        if (datetime.datetime.strptime(hora, '%H:%M').hour < 6 or datetime.datetime.strptime(hora, '%H:%M').hour >18):
+            raise Exception("Las Horas de reserva deben estarentre las 06:00 y las 18:00")  
+        
     def algoritmo_Marzullo(self,intervalos):
         tabla = []
-    
+        hora= Marzullo()
+        
         for ini,fin in intervalos:
+            hora.horaValida(ini)
+            hora.horaValida(fin)
+            ini =datetime.datetime.strptime(ini, '%H:%M').hour
+            fin =datetime.datetime.strptime(fin, '%H:%M').hour 
             tabla.append((ini,-1))
             tabla.append((fin,+1))
      
@@ -29,4 +46,7 @@ class Marzullo:
                 best = cnt
                 beststart = tabla[i][0]
                 bestend   = tabla[i+1][0]
-        return (beststart, bestend)
+        beststart = datetime.time(beststart)
+        bestend = datetime.time(bestend)
+        return (beststart.strftime('%H:%M'), bestend.strftime('%H:%M'))
+        
