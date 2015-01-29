67c2d1b58d37c30ab0d2089288344789b9e5a54a
fernandozerpa <zerpa.fernando48@gmail.com>
2015-01-29 00:36:29 -0430

    Refactorizacion en casos de prueba para marzullo

--- Tarea3/pruebaMarzullo.py
+++ Tarea3/pruebaMarzullo.py
@@ -12,23 +12,15 @@ from marzullo import Marzullo
 class PruebaMarzullo(unittest.TestCase):
 
 
-    def testPrueba1(self):
+    def testPruebaSimple(self):
         prueba = Marzullo()
         self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('11:00','13:00'),('10:00','12:00'))), ('11:00','12:00')) 
     
-    def testPrueba2(self):
+    def testDosIntervalosMismaCantidadDePuestos(self):
         prueba = Marzullo()
         self.assertEqual(prueba.algoritmo_Marzullo((('08:00','9:00'),('08:00','12:00'),('10:00','12:00'))), ('08:00','09:00')) 
     
-    def testPrueba3(self):
-        prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('09:00','10:00'),('11:00','13:00'),('10:00','12:00'))), ('11:00','12:00')) 
-    
-    def testPrueba4(self):
-        prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('09:00','10:00'),('11:00','13:00'),('14:00','15:00'))), ('09:00','10:00')) 
-    
-    def testPrueba5(self):
+    def testOffsetIgualesTypeOpuestos(self):
         prueba = Marzullo()
         self.assertEqual(prueba.algoritmo_Marzullo((('11:00','15:00'),('08:00','15:00'),('9:00','11:00'),('10:00','14:00'),('11:00','14:00'),('09:00','10:00'),('09:00','13:00'),('12:00','15:00'),('08:00','11:00'),('14:00','15:00'))), ('12:00','13:00'))
 
