779191deba3964ad63bde163cab918ba66695335
fernandozerpa <zerpa.fernando48@gmail.com>
2015-01-29 00:17:22 -0430

    se cambio el formato de las horas

--- Tarea3/pruebaMarzullo.py
+++ Tarea3/pruebaMarzullo.py
@@ -14,23 +14,23 @@ class PruebaMarzullo(unittest.TestCase):
 
     def testPrueba1(self):
         prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo(((8,12),(11,13),(10,12))), (11,12)) 
+        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('11:00','13:00'),('10:00','12:00'))), ('11:00','12:00')) 
     
     def testPrueba2(self):
         prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo(((8,9),(8,12),(10,12))), (8,9)) 
+        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','9:00'),('08:00','12:00'),('10:00','12:00'))), ('08:00','09:00')) 
     
     def testPrueba3(self):
         prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo(((8,12),(9,10),(11,13),(10,12))), (11,12)) 
+        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('09:00','10:00'),('11:00','13:00'),('10:00','12:00'))), ('11:00','12:00')) 
     
     def testPrueba4(self):
         prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo(((8,12),(9,10),(11,13),(14,15))), (9,10)) 
+        self.assertEqual(prueba.algoritmo_Marzullo((('08:00','12:00'),('09:00','10:00'),('11:00','13:00'),('14:00','15:00'))), ('09:00','10:00')) 
     
     def testPrueba5(self):
         prueba = Marzullo()
-        self.assertEqual(prueba.algoritmo_Marzullo(((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15))), (12,13)) 
+        self.assertEqual(prueba.algoritmo_Marzullo((('11:00','15:00'),('08:00','15:00'),('9:00','11:00'),('10:00','14:00'),('11:00','14:00'),('09:00','10:00'),('09:00','13:00'),('12:00','15:00'),('08:00','11:00'),('14:00','15:00'))), ('12:00','13:00'))
 
 
 if __name__ == "__main__":
